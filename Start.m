fb7cec0c82c9d95cae554908af669d0d7b8d3c83
AARON668PAN <142201609+AARON668PAN@users.noreply.github.com>
2024-04-07 15:34:56 +0800

    Add files via upload

---
+++ Start.m
@@ -0,0 +1,9 @@
+clc;clear;
+
+Specification(7)
+
+[Newrobot, ArmInfo] = importrobot('Model');
+
+wp = [ 0    0        0.1     0.3     0.3     0.1      0.1     0;
+       0    0        0.1     0.1     0.1     0.1      0.1     0.312;
+       0.74 0.74     0.55    0.55    0.35    0.35     0.55    0.312];
